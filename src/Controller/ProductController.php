ae9b7ea366a654cc64186dada9c3ac56ec90c7c7
JesseJr1 <tobias.jess@gmail.com>
2023-01-30 10:36:29 +0100

    WIP on form: need to see Rate on form in product.detail

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Form\ReviewType;
 use App\Repository\ProductRepository;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,10 +31,33 @@ class ProductController extends AbstractController
     }
 
     #[Route('/produits{id<\d+>}', name: 'app_product_detail')]
-    public function detail(string $id, ProductRepository $productRepository)
+    public function details(ProductRepository $productRepository, string $id, Request $request, EntityManagerInterface $manager, ReviewRepository $reviewRepository): Response
     {
+        $product = $productRepository->find($id);
+        if (!$product) {
+            // throw $this-> createNotFoundException();
+        }
+        $review = new Review;
+        $reviewForm = $this->createForm(ReviewType::class, $review);
+        $reviewForm->handleRequest($request);
+        
+        if ($reviewForm->isSubmitted() && $reviewForm->isValid()){
+            $review->setProduct($product);
+            /* $review->setContent($content); */
+            /* $review->setEmail($email); */
+            
+            $manager->persist($review);
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre message est bien envoyé!!!');
+            return $this->redirectToRoute('app_product_detail'); 
+            
+        }
         return $this->render('product/detail.html.twig', [
-            'product_detail' => $productRepository->find($id),
+            'product' => $product,
+            'reviewForm' => $reviewForm->createView(),
+            'product' => $reviewRepository->findBy(['product'=>$id])
         ]);
+
     }
 }
